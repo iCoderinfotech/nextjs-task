1ed8a6beb0da3786430253225a5a3c0d5913077f
Jaynam Dalal <125733339+iCoderinfotech@users.noreply.github.com>
2025-01-14 23:17:35 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    images: {
+        domains: ['localhost'], // Add your domain here if the image is not local
+      },
+
+};
+
+export default nextConfig;
